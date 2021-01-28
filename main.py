864a7bb90d96b6324c4bbfdd0214b112e9e9ba19
Thomas Povinelli <tompov227@gmail.com>
2021-01-27 21:49:21 -0500

    Formatting

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ silent = False
 fps = 8
 headers = False
 
+
 def parse_args():
     ap = argparse.ArgumentParser()
     ap.add_argument('video', help='the video to use')
@@ -26,8 +27,10 @@ def parse_args():
     ap.add_argument('-r', '--headers', action='store_true', help='Print response headers')
     return ap.parse_args()
 
+
 def last_path(path):
     return os.path.split(path)[-1]
+
 
 def filename_to_parts(name):
     if '.' not in name:
@@ -35,6 +38,7 @@ def filename_to_parts(name):
     else:
         *names, ext = name.rsplit('.')
         return '.'.join(names), ext
+
 
 def ensure_dir(videoname):
     if os.path.isdir(videoname):
@@ -44,6 +48,7 @@ def ensure_dir(videoname):
     else:
         os.mkdir(videoname)
 
+
 def convert_video_to_frames(videopath, fps):
     videoname, videoext = filename_to_parts(last_path(videopath))
     ensure_dir(videoname)
@@ -51,6 +56,7 @@ def convert_video_to_frames(videopath, fps):
     status, output = subprocess.getstatusoutput(command)
     if verbose:
         print(output)
+
 
 def scale_image(impath):
     i = cv2.imread(impath)
@@ -64,6 +70,7 @@ def scale_image(impath):
     cv2.imwrite(impath, result)
     return True
 
+
 def scale_video_frames(videopath, n=3):
     name, ext = filename_to_parts(last_path(videopath))
     namesfmt = '{}/%0{}d.bmp'.format(name, n)
@@ -75,29 +82,29 @@ def scale_video_frames(videopath, n=3):
     input("press enter!")
 
 
-colors = {
-  0: '\u001b[40;1m',
-  (1 << 2): '\u001b[41;1m',
-  (1 << 1): '\u001b[42;1m',
-  ((1 << 2) | (1 << 1)): '\u001b[43;1m',
-  (1 << 0): '\u001b[44;1m',
-  ((1 << 2) | (1 << 0)): '\u001b[45;1m',
-  ((1 << 1) | (1 << 0)): '\u001b[46;1m',
-  7: '\u001b[47;1m'
-}
-
 def hex_to_ansi(hex):
     r, g, b = hex[1:3], hex[3:5], hex[5:]
     r, g, b = int(r, base=16), int(g, base=16), int(b, base=16)
     r, g, b = r // 127, g // 127, b // 127
     index = (r << 2) | (g << 1) | (b << 0)
-    return colors[index]
+    return ({
+            0: '\u001b[40;1m',
+            (1 << 2): '\u001b[41;1m',
+            (1 << 1): '\u001b[42;1m',
+            ((1 << 2) | (1 << 1)): '\u001b[43;1m',
+            (1 << 0): '\u001b[44;1m',
+            ((1 << 2) | (1 << 0)): '\u001b[45;1m',
+            ((1 << 1) | (1 << 0)): '\u001b[46;1m',
+            7: '\u001b[47;1m'
+            })[index]
+
 
 def color_from_style(style):
-    r =  style.split(':')[-1].strip(';')
+    r = style.split(':')[-1].strip(';')
     if verbose:
         print(r)
     return r
+
 
 def ascify_image(impath):
     key = dotenv.get_key('./.env', 'X_TEXTART_API_SECRET')
@@ -121,6 +128,7 @@ def ascify_image(impath):
         s += (hex_to_ansi(clr) + " ")
     return s
 
+
 def print_video_frames(videopath, n=3):
     name, ext = filename_to_parts(last_path(videopath))
     namesfmt = '{}/%0{}d.bmp'.format(name, n)
@@ -138,7 +146,6 @@ def print_video_frames(videopath, n=3):
                 print("Out of frames? " + str(e))
 
 
-
 def main():
     global verbose, silent, fps, headers
     options = parse_args()
@@ -150,6 +157,7 @@ def main():
     if not options.convert:
         scale_video_frames(options.video)
     print_video_frames(options.video)
+
 
 if __name__ == '__main__':
     exit(main())
